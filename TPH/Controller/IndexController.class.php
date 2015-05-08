b18e209465a5b72aa83ef476428f638c705bfbd0
fish <zhuanqianfish@gmail.com>
2015-05-09 00:46:10 +0800

    cheage version to 0.34,add form code functions

--- TPH/Controller/IndexController.class.php
+++ TPH/Controller/IndexController.class.php
@@ -1,6 +1,6 @@
 <?php
 //	ThinkphpHelper v0.3
-//	
+//
 //	weiyunstudio.com
 //	sjj zhuanqianfish@gmail.com
 //	2014年9月18日
@@ -12,22 +12,22 @@ class IndexController extends Controller {
     public function index(){
 		$this->display("Public/help");
     }
-	
+
 	public function ui(){
 		$this->display("Public/ui");
     }
-	
+
 	public function help(){
 		$this->display("Public/help");
     }
-	
+
 	public function test(){
 		$this->display();
 	}
-	
+
 	public function checkVersion(){
 		header("Content-type: text/html; charset=utf-8");
-		$version = 0.331;
+		$version = 0.34;
 		$url = 'http://www.weiyunstudio.com/thinkphpHelper/version.php';
 		$newVersion =  (float)file_get_contents($url);
 		if($newVersion > $version){
