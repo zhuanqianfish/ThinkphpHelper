923a151b8404fc400dc171cd777dd04fad595731
zhuanqianfish <zhuanqianfish@gmail.com>
2017-08-19 12:18:12 +0800

    modefy view dect
    修改了view代码模板的中文字典功能

--- TPH/Conf/config.php
+++ TPH/Conf/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
-	'VERSION'	=> 0.35,	//版本号
+	'VERSION'	=> 0.36,	//版本号
+	'PROJECT_NAME'	=>	'',		//项目名
+	'PROJECT_DESCRIPTION'	=>'',	//项目描述
 	'LOAD_EXT_CONFIG' => 'dict', 
-		
 );
